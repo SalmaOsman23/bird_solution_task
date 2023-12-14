96f93ba16daa0bfa5848bfa299bb81ea3e5df9ac
Salma <salmaosmann21@gmail.com>
2023-12-14 05:40:54 +0200

    Authentication

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http/http.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String routeName = "Login Screen";
@@ -14,6 +16,8 @@ class _LoginScreenState extends State<LoginScreen> {
 
   var passwordController = TextEditingController();
 
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   bool _rememberMe = false;
 
   @override
@@ -21,189 +25,210 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       backgroundColor: const Color(0xffF5F5FA),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.width * 0.4,
-            ),
-            const Text(
-              "Welcome Back!",
-              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
-            ),
-            const Text(
-              "Login to continue Radio App",
-              style: TextStyle(fontWeight: FontWeight.w400, fontSize: 16),
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  child: TextFormField(
-                    style: const TextStyle(color: Colors.grey),
-                    controller: emailController,
-                    decoration: InputDecoration(
-                      labelText: "Email Address",
-                      fillColor: Colors.white,
-                      labelStyle: const TextStyle(color: Colors.grey),
-                      filled: true,
-                      prefixIcon: const Icon(
-                        Icons.email_outlined,
-                        color: Colors.grey,
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: const BorderSide(color: Colors.grey),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: const BorderSide(color: Colors.grey),
-                      ),
-                    ),
-                  ),
-                ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.4,
               ),
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  child: TextFormField(
-                    style: const TextStyle(color: Colors.grey),
-                    controller: passwordController,
-                    decoration: InputDecoration(
-                      labelText: "Password",
-                      fillColor: Colors.white,
-                      labelStyle: const TextStyle(color: Colors.grey),
-                      filled: true,
-                      prefixIcon: const Icon(
-                        Icons.lock_outline_rounded,
-                        color: Colors.grey,
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: const BorderSide(color: Colors.grey),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: const BorderSide(color: Colors.grey),
-                      ),
-                    ),
-                  ),
-                ),
+              const Text(
+                "Welcome Back!",
+                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Row(
-                  children: [
-                    Checkbox(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0),
+              const Text(
+                "Login to continue Radio App",
+                style: TextStyle(fontWeight: FontWeight.w400, fontSize: 16),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    child: TextFormField(
+                      style: const TextStyle(color: Colors.grey),
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        labelText: "Email Address",
+                        fillColor: Colors.white,
+                        labelStyle: const TextStyle(color: Colors.grey),
+                        filled: true,
+                        prefixIcon: const Icon(
+                          Icons.email_outlined,
+                          color: Colors.grey,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: const BorderSide(color: Colors.grey),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: const BorderSide(color: Colors.grey),
+                        ),
                       ),
-                      checkColor: Colors.red,
-                      activeColor: Colors.white,
-                      value: _rememberMe,
-                      onChanged: (value) {
-                        setState(() {
-                          _rememberMe = value ?? false;
-                        });
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your email address.';
+                        }
+                        return null;
                       },
                     ),
-                    const Text('Remember Me'),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    child: TextFormField(
+                      style: const TextStyle(color: Colors.grey),
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        fillColor: Colors.white,
+                        labelStyle: const TextStyle(color: Colors.grey),
+                        filled: true,
+                        prefixIcon: const Icon(
+                          Icons.lock_outline_rounded,
+                          color: Colors.grey,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: const BorderSide(color: Colors.grey),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: const BorderSide(color: Colors.grey),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your password.';
+                        } else if (value.length < 6) {
+                          return 'Password must be at least 6 characters.';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    children: [
+                      Checkbox(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        checkColor: Colors.red,
+                        activeColor: Colors.white,
+                        value: _rememberMe,
+                        onChanged: (value) {
+                          setState(() {
+                            _rememberMe = value ?? false;
+                          });
+                        },
+                      ),
+                      const Text('Remember Me'),
+                    ],
+                  ),
+                  const InkWell(child: Text("Forget password?")),
+                ],
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Handle button tap
+                    if (_formKey.currentState?.validate() ?? false) {
+                      login(emailController.text.toString(),
+                          passwordController.text.toString());
+                    }
+                    print('Button tapped');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: const Color(0xff7A5FC9),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    elevation: 5.0,
+                  ),
+                  child: const Text(
+                    'Log In',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(18.0),
+                child: Text(
+                  "OR",
+                  style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20),
+                ),
+              ),
+              customButton(
+                buttonColor: Colors.white,
+                titleColor: Colors.black,
+                buttonTitle: "Continue with Google",
+                imageAsset: "assets/images/google_logo.png",
+                onClick: () {},
+              ),
+              customButton(
+                buttonColor: Colors.black,
+                titleColor: Colors.white,
+                buttonTitle: "Continue with Apple ID",
+                imageAsset: "assets/images/apple_logo.png",
+                onClick: () {},
+              ),
+              customButton(
+                buttonColor: const Color(0xff3B5998),
+                titleColor: Colors.white,
+                buttonTitle: "Continue with Facebook ",
+                imageAsset: "assets/images/facebook_logo.png",
+                onClick: () {},
+              ),
+              const Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account?",
+                      style: TextStyle(fontWeight: FontWeight.w600),
+                    ),
+                    InkWell(
+                      child:
+                          Text("Sign up", style: TextStyle(color: Colors.blue)),
+                    ),
                   ],
                 ),
-                const InkWell(child: Text("Forget password?")),
-              ],
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.8,
-              height: 50,
-              child: ElevatedButton(
-                onPressed: () {
-                  // Handle button tap
-                  print('Button tapped');
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: const Color(0xff7A5FC9),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  elevation: 5.0,
-                ),
-                child: const Text(
-                  'Log In',
-                  style: TextStyle(color: Colors.white, fontSize: 18),
-                ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(18.0),
-              child: Text(
-                "OR",
-                style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20),
+              const Text(
+                "By signing up you indicate that you have read and",
+                style: TextStyle(fontSize: 10),
               ),
-            ),
-            customButton(
-              buttonColor: Colors.white,
-              titleColor: Colors.black,
-              buttonTitle: "Continue with Google",
-              imageAsset: "assets/images/google_logo.png",
-              onClick: () {},
-            ),
-            customButton(
-              buttonColor: Colors.black,
-              titleColor: Colors.white,
-              buttonTitle: "Continue with Apple ID",
-              imageAsset: "assets/images/apple_logo.png",
-              onClick: () {},
-            ),
-            customButton(
-              buttonColor: const Color(0xff3B5998),
-              titleColor: Colors.white,
-              buttonTitle: "Continue with Facebook ",
-              imageAsset: "assets/images/facebook_logo.png",
-              onClick: () {},
-            ),
-            const Padding(
-              padding: EdgeInsets.all(12.0),
-              child: Row(
+              const Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
-                    "Don't have an account?",
-                    style: TextStyle(fontWeight: FontWeight.w600),
+                    "agreed to the patch",
+                    style: TextStyle(fontSize: 10),
                   ),
-                  InkWell(
-                    child:
-                        Text("Sign up", style: TextStyle(color: Colors.blue)),
-                  ),
+                  Text("Terms of Service",
+                      style: TextStyle(color: Colors.blue, fontSize: 10))
                 ],
               ),
-            ),
-            const Text(
-              "By signing up you indicate that you have read and",
-              style: TextStyle(fontSize: 10),
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "agreed to the patch",
-                  style: TextStyle(fontSize: 10),
-                ),
-                Text("Terms of Service",
-                    style: TextStyle(color: Colors.blue, fontSize: 10))
-              ],
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.width * 0.4,
-            ),
-          ],
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.4,
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -252,6 +277,44 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
     );
   }
+
+  void login(String email, String password) async {
+    try {
+      Response response = await post(
+          Uri.parse('https://my-safe-space.alacrity.technology/api/login'),
+          body: {'email': email, 'password': password});
+      if (response.statusCode >= 200 && response.statusCode < 300) {
+        //make sure to not add extra spaces while typing email or password
+        Fluttertoast.showToast(
+          msg: "Account created",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.green,
+          textColor: Colors.white,
+          fontSize: 16.0,
+        );
+      } else {
+        Fluttertoast.showToast(
+          msg: "Failed to create an account",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.green,
+          textColor: Colors.white,
+          fontSize: 16.0,
+        );
+      }
+    } catch (e) {
+      Fluttertoast.showToast(
+        msg: "Error: ${e.toString()}",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.green,
+        textColor: Colors.white,
+        fontSize: 16.0,
+      );
+    }
+  }
 }
-
-
